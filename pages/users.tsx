d43403515c4bcd38a341a02115f9c3958b521288
Yash Patel <yash1451999@gmail.com>
2022-12-07 10:49:33 +0530

    bootstrap table added

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,3 +1,5 @@
+import DataTable from "react-data-table-component";
+import { TableColumn } from "react-data-table-component/dist/src/DataTable/types";
 import Navbar from "../components/navbar";
 import { GETAPI, responseData } from "../constants/API";
 export async function getStaticProps() {
@@ -8,16 +10,54 @@ export async function getStaticProps() {
     },
   };
 }
-export default function Users({ userList }: { userList: responseData }) {
-  console.log(userList);
 
+const columns = [
+  {
+    name: "#",
+    selector: "serial",
+  },
+  {
+    name: "Name",
+    selector: "name",
+    sortable: true,
+  },
+  {
+    name: "Email",
+    selector: "email",
+    sortable: true,
+  },
+  {
+    name: "Role",
+    selector: "role.name",
+    sortable: true,
+  },
+  {
+    name: "Created At",
+    selector: "createdAt",
+    sortable: true,
+  },
+] as unknown as TableColumn<unknown>[];
+
+export default function Users({ userList }: { userList: responseData }) {
+  userList.data.forEach((user: any, index: any) => {
+    user.serial = index + 1;
+    user.createdAt = user.createdAt.substring(0, 10);
+  });
   return (
     <>
       <Navbar />
-      <ul>
-        {userList.success &&
-          userList.data.map((item: any) => <li>{item.name}</li>)}
-      </ul>
+      {userList.success && (
+        <div className="mx-x px-5">
+          <DataTable
+            title="Users"
+            columns={columns}
+            data={userList.data}
+            pagination
+            highlightOnHover
+            className="border"
+          />
+        </div>
+      )}
     </>
   );
 }
